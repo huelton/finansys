69e0b641a366dd561454777632073fb705134a14
huelton <hueltondihon@gmail.com>
2020-03-10 23:12:02 -0300

    add category model updated

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,5 +1,20 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Category } from "./pages/categories/shared/category.model";
+
 
 export class InMemoryDatabase implements InMemoryDbService {
-    
+
+   createDb(){
+     const categories: Category[] = [
+        { id: 1, name:'Moradia', description: 'Pagamentos de Contas da Casa' },
+        { id: 2, name:'Saude', description: 'Plano de Saude e Remedios' },
+        { id: 3, name:'Lazer', description: 'Cinema, Parques, Praia, etc' },
+        { id: 4, name:'Salario', description: 'Recebimento de Salario' },
+        { id: 5, name:'Freelas', description: 'Trabalhos como Freelancer' }
+        { id:, name}
+     ];
+
+     return { categories }
+   }
+
 }
