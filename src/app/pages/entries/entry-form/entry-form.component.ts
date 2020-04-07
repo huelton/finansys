1286c2c96cc1ccb2469ff399847aa582a9eb9a7a
huelton <hueltondihon@gmail.com>
2020-04-07 17:37:15 -0300

    improving EntryFormComponent

--- src/app/pages/entries/entry-form/entry-form.component.ts
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { Entry } from '../shared/entry.model';
 import { EntryService } from '../shared/entry.service';
+import { Category } from '../../categories/shared/category.model';
+import { CategoryService } from '../../categories/shared/category.service';
 
 import { switchMap } from 'rxjs/operators';
 import toastr from 'toastr';
@@ -21,18 +23,43 @@ pageTitle: string;
 serverErrorMessages: string[] = null;
 submittingForm: boolean = false;
 entry: Entry = new Entry();
+categories: Array<Category>
+
+imaskConfig = {
+  mask: Number,
+  scale: 2,
+  thousandsSeparator: '',
+  padFractionalZeros: true,
+  normalizeZeros: true,
+  radix: ','
+};
+
+ptBR = {
+
+  firstDayOfWeek: 0,
+  dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sabado'],
+  dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+  dayNamesMin: ['Do','Se','Te','Qu','Qi','Sx','Sa'],
+  monthNames: ['Janeiro','Feveiro','Marco','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+  monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+  today: "Hoje",
+  clear: "Limpar"
+
+}
 
   constructor(
     private entryService: EntryService,
     private route: ActivatedRoute,
     private router: Router,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private categoryService: CategoryService
   ) { }
 
   ngOnInit() {
     this.setCurrentAction();
     this.buildEntryForm();
     this.loadEntry();
+    this.loadCategories();
   }
 
   ngAfterContentChecked(){
@@ -48,7 +75,24 @@ submitForm() {
     this.updateEntry();
 }
 
+get typeOptions(): Array<any> {
+  return Object.entries(Entry.types).map(
+      ([value, text]) => {
+        return {
+          text: text,
+          value: value
+        }
+      }
+   )
+}
+
   // Private Methods
+
+  private loadCategories() {
+    this.categoryService.getAll().subscribe(
+      categories => this.categories = categories;
+    )
+  }
 
   private setCurrentAction() {
     if(this.route.snapshot.url[0].path == 'new'){
@@ -63,10 +107,10 @@ submitForm() {
       id: [null],
       name: [null, [Validators.required, Validators.minLength(2)]],
       description: [null],
-      type: [null, [Validators.required]],
+      type: ["expense", [Validators.required]],
       amount: [null, [Validators.required]],
       date: [null, [Validators.required]],
-      paid: [null, [Validators.required]],
+      paid: [true, [Validators.required]],
       categoryId: [null, [Validators.required]]
 
     });
