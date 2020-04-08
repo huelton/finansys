30dc4147d095a5f72169f3f0960c8905f6b15d29
huelton <hueltondihon@gmail.com>
2020-04-08 18:33:17 -0300

    improving EntryService

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -17,21 +17,21 @@ import { Entry } from './entry.model';
  }
 
  create(entry: Entry): Observable<Entry> {
-   return this.categoryService.getById(entry.categoryId).pipe(
-     flatMap(category => {
-       entry.category = category;
-       return super.create(entry)
-     })
-   )
+   return this.setCategoryAndSendToServer(entry, super.create.bind(this));
  }
 
  update(entry: Entry): Observable<Entry> {
-     return this.categoryService.getById(entry.categoryId).pipe(
-       flatMap(category => {
-         entry.category = category;
-         return super.update(entry)
-       })
-     )
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this));
+ }
+
+ private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry>{
+   return this.categoryService.getById(entry.categoryId).pipe(
+     flatMap(category => {
+       entry.category = category;
+       return sendFn(entry)
+     }),
+     catchError(this.handleError)
+   );
  }
 
 }
